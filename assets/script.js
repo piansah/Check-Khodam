71a5ce70157a9a8229687d7d18570e7797fb451d
Pian <alifalpian157@gmail.com>
2024-06-30 13:22:18 +0700

    refactor

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
 // script.js
 function cekKhodam(event) {
    event.preventDefault();
-   const nama = document.getElementById('search').value;
+   const nama = document.getElementById('search').value.trim(); // Trim to remove leading/trailing spaces
+   
    if (nama === '') {
        alert('Nama kisanak tidak boleh kosong!');
    } else {
-      // const status = ['isi', 'kosong'];
-      // const randomStatus = Math.floor(Math.random() * status.length);
       const khodam = [
           'Kodok',
           'Katak',
@@ -22,14 +21,17 @@ function cekKhodam(event) {
           'Sapi'
       ];
       const randomKhodam = Math.floor(Math.random() * khodam.length);
-      const resultText = `${nama.toUpperCase()} - Khodam Kamu ${khodam[randomKhodam]}`
-      // const resultText = randomStatus === 0 
-      //     ? `${nama.toUpperCase()} - Khodam Kamu ${khodam[randomKhodam]}`
-      //     : `${nama.toUpperCase()} - Kamu tidak memiliki Khodam`;
+      const resultText = `${nama.toUpperCase()} - Khodam Kamu ${khodam[randomKhodam]}`;
       
-      document.getElementById('result').innerHTML = resultText;
+      // Update result element
+      const resultElement = document.getElementById('result');
+      resultElement.textContent = resultText;
+      resultElement.style.color = 'white'; 
+      resultElement.style.textShadow = 'black';
+      resultElement.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 5)'; 
+
+      
       document.getElementById('search').value = '';
-      
    }
 }
 
